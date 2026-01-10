1481094f0ae2c0911d34899f00c418b67da81c09
Debdulal Sahoo <sahoodebdulal007@gmail.com>
2026-01-10 16:10:09 +0000

    Fix Firebase Admin syntax error

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert({
+import admin from "firebase-admin";
+
+admin.initializeApp({
+  credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-  )}
- }); 
- /*ivateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  })/*
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
+
 
 
 const db = admin.firestore();
